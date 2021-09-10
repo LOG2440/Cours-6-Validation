29f2788a9bd90626cec7388f2f3dde64869fcc99
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2021-09-10 12:35:17 -0400

    Added example for JSDoc comments.

--- anagram_test/anagram.js
+++ anagram_test/anagram.js
@@ -1,3 +1,11 @@
+/**
+ * Compares two strings to see if they are anagrams of each other
+ * An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+ * typically using all the original letters exactly once
+ * @param {string} firstSequence original word to compare to
+ * @param {string} secondSequence possible anagram
+ * @returns {boolean} returns true if secondSequence is an anagram of firstSequence
+ */
 function anagram(firstSequence, secondSequence) {
   if (firstSequence.length !== secondSequence.length) {
     return false;
